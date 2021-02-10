6bfdc970669815a3a2a231ed744e4a9a086e79fe
Martin Acosta <tinchoz49@gmail.com>
2021-02-10 16:40:46 -0300

    first commit

--- src/swarm.js
+++ src/swarm.js
@@ -10,55 +10,105 @@ export function Swarm ({ id = 'default', config = {}, children }) {
   const { swarms } = useContext(SwarmContext)
 
   useDeepCompareEffect(() => {
-    const swarm = discoverySwarmWebrtc(config)
-    swarms.set(id, swarm)
+    const ref = swarms.get(id) || {
+      topicHandlers: new Map(),
+      topicReplicate: new Map()
+    }
+
+    const swarm = discoverySwarmWebrtc({
+      ...config,
+      stream: (info) => {
+        const replicate = ref.topicReplicate.get(info.channel.toString('hex'))
+        if (replicate) return replicate(info)
+        return config.stream && config.stream(info)
+      }
+    })
+
+    function onConnection (conn, info) {
+      const handler = ref.topicHandlers.get(info.channel.toString('hex'))
+      handler && handler('connection', conn, info)
+    }
+
+    function onDisconnection (conn, info) {
+      const handler = ref.topicHandlers.get(info.channel.toString('hex'))
+      handler && handler('disconnection', conn, info)
+    }
+
+    swarm.on('connection', onConnection)
+    swarm.on('connection-closed', onDisconnection)
+
+    ref.swarm = swarm
+    swarms.set(id, ref)
     setSwarm(swarm)
+
+    return function close () {
+      swarm.removeListener('connection', onConnection)
+      swarm.removeListener('connection-closed', onDisconnection)
+      swarm.close().catch(err => {
+        console.error(err)
+      })
+    }
   }, [config])
 
   return (swarm ? children : null)
 }
 
-export function useJoin ({ topic, id, replicator }) {
-  const { swarm } = useSwarm({ id, replicator })
+export function useJoin ({ topic, id, replicate, onConnection, onDisconnection }) {
+  const { swarms } = useContext(SwarmContext)
+  const ref = swarms.get(id) || {}
+
   const [peers, setPeers] = useState([])
 
+  const topicStr = topic.toString('hex')
+
   useEffect(() => {
+    const { swarm } = ref
+    if (!swarm) return
+
     swarm.join(topic)
 
-    function connectionHandler (_, info) {
-      if (info.channel.toString('hex') !== topic.toString('hex')) return
-
-      setPeers(swarm.getPeers(Buffer.from(topic, 'hex')))
-    }
-
-    swarm.on('connection', connectionHandler)
-    swarm.on('connection-closed', connectionHandler)
-
     return function leave () {
-      swarm.removeListener('connection', connectionHandler)
-      swarm.removeListener('connection-closed', connectionHandler)
-
       swarm.leave(topic)
     }
-  }, [topic.toString('hex')])
-
-  return { swarm, peers }
-}
-
-export function useSwarm ({ id = 'default', replicator } = {}) {
-  const { swarms } = useContext(SwarmContext)
-
-  const swarm = swarms.get(id)
+  }, [topicStr, ref.swarm])
 
   useEffect(() => {
-    if (!replicator) return
+    const { swarm, topicHandlers } = ref
+    if (!swarm || !topicHandlers) return
 
-    swarm.on('connection', replicator)
+    topicHandlers.set(topicStr, (type, conn, info) => {
+      if (type === 'connection') {
+        onConnection && onConnection(conn, info)
+      } else {
+        onDisconnection && onDisconnection(conn, info)
+      }
+      setPeers(swarm.getPeers(topic))
+    })
 
-    return function () {
-      swarm.removeListener('connection', replicator)
+    return () => {
+      topicHandlers.delete(topicStr)
     }
-  }, [id, replicator])
+  }, [topicStr, onConnection, onDisconnection])
 
-  return { swarm }
+  useEffect(() => {
+    const { swarm, topicReplicate } = ref
+    if (!swarm || !topicReplicate) return
+
+    topicReplicate.set(topicStr, info => replicate(info))
+
+    return () => {
+      topicReplicate.delete(topicStr)
+    }
+  }, [topicStr, replicate])
+
+  return { peers, swarm: ref.swarm }
+}
+
+export function useSwarm ({ id = 'default' } = {}) {
+  const { swarms } = useContext(SwarmContext)
+
+  const ref = swarms.get(id)
+  if (!ref) return { swarm: undefined }
+
+  return { swarm: ref.swarm }
 }
